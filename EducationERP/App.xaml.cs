db349d8ce500c82fc74606b1b0c96b2df8b2ba3d
EddoCoding <codingeprst@gmail.com>
2024-07-17 20:13:46 +0300

    Добавление ссылки на проект

--- EducationERP/App.xaml.cs
+++ EducationERP/App.xaml.cs
@@ -4,9 +4,6 @@ namespace EducationERP
 {
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-        }
+        protected override void OnStartup(StartupEventArgs e) => base.OnStartup(e);
     }
 }
