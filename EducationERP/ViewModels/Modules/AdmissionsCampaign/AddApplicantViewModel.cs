0c06844c6a1d4e8f352c30392c7f526b3547c7a8
EddoCoding <codingeprst@gmail.com>
2024-09-08 21:57:26 +0300

    Отрисована часть UserControlа создания личного дела.

--- EducationERP/ViewModels/Modules/AdmissionsCampaign/AddApplicantViewModel.cs
+++ EducationERP/ViewModels/Modules/AdmissionsCampaign/AddApplicantViewModel.cs
@@ -1,4 +1,5 @@
-﻿using EducationERP.Models;
+﻿using EducationERP.Common.Components.Services;
+using EducationERP.Models;
 using Raketa;
 
 namespace EducationERP.ViewModels.Modules.AdmissionsCampaign
@@ -6,5 +7,17 @@ namespace EducationERP.ViewModels.Modules.AdmissionsCampaign
     public  class AddApplicantViewModel : RaketaViewModel
     {
         public Applicant Applicant { get; set; } = new();
+
+        public RaketaCommand ExitCommand { get; set; }
+
+        ITabControl _tabControl;
+        public AddApplicantViewModel(ITabControl tabControl)
+        {
+            _tabControl = tabControl;
+
+            ExitCommand = RaketaCommand.Launch(CloseTab);
+        }
+
+        void CloseTab() => _tabControl.RemoveTab();
     }
 }
