3ed62248e629a98309ece1ce3493c810a6a7dd05
EddoCoding <codingeprst@gmail.com>
2024-09-16 22:29:00 +0300

    Сделано окно добавления ЕГЭ, сделана ViewModel для окна ЕГЭ, сделана VIewModel самого ЕГЭ

---
+++ EducationERP/ViewModels/Modules/AdmissionsCampaign/Exams/EGEViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Raketa;
+using System.Collections.ObjectModel;
+
+namespace EducationERP.ViewModels.Modules.AdmissionsCampaign.Exams
+{
+    public class EGEViewModel : RaketaViewModel
+    {
+        public EGEVM EGE { get; set; } = new();
+
+        ObservableCollection<EGEVM> _eges;
+
+        public RaketaTCommand<EGEVM> AddEGECommand { get; set; }
+        public RaketaCommand ExitCommand { get; set; }
+
+        IServiceView _serviceView;
+        public EGEViewModel(IServiceView serviceView, ObservableCollection<EGEVM> EGES)
+        {
+            _serviceView = serviceView;
+            _eges = EGES;
+
+            AddEGECommand = RaketaTCommand<EGEVM>.Launch(AddEGE);
+            ExitCommand = RaketaCommand.Launch(ExitLogin);
+
+        }
+
+        void AddEGE(EGEVM ege)
+        {
+            var isValidated = ege.Validation();
+            if (isValidated)
+            {
+                _eges.Add(ege);
+                _eges = null;
+                _serviceView.Close<EGEViewModel>();
+            }
+        }
+        void ExitLogin() => _serviceView.Close<EGEViewModel>();
+    }
+}
