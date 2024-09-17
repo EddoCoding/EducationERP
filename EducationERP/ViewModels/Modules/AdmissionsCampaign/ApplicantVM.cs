da6236c15a3e506ac3021e44d08e81e24ad47c14
EddoCoding <codingeprst@gmail.com>
2024-09-17 13:38:33 +0300

    Провел небольшой рефакторинг

--- EducationERP/ViewModels/Modules/AdmissionsCampaign/ApplicantVM.cs
+++ EducationERP/ViewModels/Modules/AdmissionsCampaign/ApplicantVM.cs
@@ -1,9 +1,13 @@
-﻿using Raketa;
+﻿using EducationERP.ViewModels.Modules.AdmissionsCampaign.Documents;
+using EducationERP.ViewModels.Modules.AdmissionsCampaign.Education;
+using EducationERP.ViewModels.Modules.AdmissionsCampaign.Exams;
+using Raketa;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace EducationERP.ViewModels.Modules.AdmissionsCampaign
 {
-    public class ApplicantVM : RaketaViewModel
+    public class ApplicantVM : RaketaViewModel, IDisposable
     {
         string citizenship = string.Empty;
         int totalPoints;
@@ -33,9 +37,9 @@ namespace EducationERP.ViewModels.Modules.AdmissionsCampaign
         public string AdditionalInformation { get; set; } = string.Empty;
 
         // Документы, Образование, Признаки и т.д.
-        public ObservableCollection<string> Documents { get; set; } = new();
-        public ObservableCollection<string> Educations { get; set; } = new();
-        public ObservableCollection<string> EGES { get; set; } = new();
+        public ObservableCollection<DocumentBaseViewModel> Documents { get; set; } = new();
+        public ObservableCollection<EducationBaseViewModel> Educations { get; set; } = new();
+        public ObservableCollection<EGEVM> EGES { get; set; } = new();
         public int TotalPoints
         {
             get => totalPoints;
@@ -45,5 +49,23 @@ namespace EducationERP.ViewModels.Modules.AdmissionsCampaign
         public ObservableCollection<string> AreasOfTraining { get; set; } = new();
         public string SelectedDirectionOfTraining { get; set; } = string.Empty;
         public ObservableCollection<string> SubmittedDocuments { get; set; } = new();
+
+
+
+        public ApplicantVM() => EGES.CollectionChanged += ChangeValueProperty;
+
+        void ChangeValueProperty(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null)
+                foreach (var newItem in e.NewItems)
+                    if (newItem is EGEVM ege) TotalPoints += ege.SubjectScores;
+
+
+            if (e.OldItems != null)
+                foreach (var oldItem in e.OldItems)
+                    if (oldItem is EGEVM ege) TotalPoints -= ege.SubjectScores;
+        }
+
+        public void Dispose() => EGES.CollectionChanged -= ChangeValueProperty;
     }
 }
