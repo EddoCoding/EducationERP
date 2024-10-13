762436afc76e5a32e15853e59e4f506b26276ad9
Eddo <codingeprst@gmail.com>
2024-10-13 16:47:57 +0300

    Добавлен конвертер TimeOnly и сделано правильное дефолтное и ошибочное отображение TimeOnly

---
+++ EducationERP/Common/Converters/TimeOnlyToTimeOnlyConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace EducationERP.Common.Converters
+{
+    public class TimeOnlyToTimeOnlyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeOnly time && time == TimeOnly.MinValue) return string.Empty;
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try { return TimeOnly.ParseExact((string)value, "HH:mm", CultureInfo.InvariantCulture); }
+            catch { return TimeOnly.MinValue; }
+        }
+    }
+}
